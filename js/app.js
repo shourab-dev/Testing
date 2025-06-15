bc8b88e4401c93705a0588157f5e1d0950167f82
abdur <abdur@gmail.com>
2025-06-15 14:45:56 +0600

    First commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.bannerSlider').slick({
+        autoplay: true,
+        autoplaySpeed:2500,
+        prevArrow: '#banner .leftArrow',
+        nextArrow: '#banner .rightArrow',
+        dots: true,
+    });
+});
+
